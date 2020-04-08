b4ce80ce734b16bd39a202915ff95f2149650340
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-04-08 22:19:07 +0100

    initial commit

---
+++ label.tf
@@ -0,0 +1,9 @@
+module "label" {
+  source             = "git::https://github.com/Callumccr/tf-mod-label.git?ref=master"
+  namespace          = var.namespace
+  environment        = var.environment
+  name               = var.name
+  attributes         = concat(var.attributes, ["endpoints"])
+  delimiter          = "-"
+  additional_tag_map = {} /* Additional attributes (e.g. 1) */
+  label_order        = ["environment", "namespace", "name", "attributes"]
